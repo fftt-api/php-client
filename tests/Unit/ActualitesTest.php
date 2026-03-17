b3e9e1eb742e2df23108f19e7f3a3646936bcd80
Aurélien Devaux <github@aureldvx.net>
2026-03-17 15:36:21 +0100

    tests: describe groups of test suites

--- tests/Unit/ActualitesTest.php
+++ tests/Unit/ActualitesTest.php
@@ -10,11 +10,13 @@ beforeEach(function (): void {
     $this->api = FFTTApi::create(appId: '', appKey: '', serial: '', httpClient: HttpClientMock::class);
 });
 
-it('devrait récupérer les dernières actualités de la fédération', function (): void {
-    $actualites = $this->api->actualites->fluxActualitesFederation();
+describe('[xml_new_actu]', function (): void {
+    it(' devrait récupérer les dernières actualités de la fédération', function (): void {
+        $actualites = $this->api->actualites->fluxActualitesFederation();
 
-    expect($actualites)->toBeArray()
-        ->and($actualites)->not->toBeEmpty()
-        ->and($actualites[0])->toBeInstanceOf(Actualite::class)
-        ->and($actualites[0]->date()->isValid())->toBeTrue();
+        expect($actualites)->toBeArray()
+            ->and($actualites)->not->toBeEmpty()
+            ->and($actualites[0])->toBeInstanceOf(Actualite::class)
+            ->and($actualites[0]->date()->isValid())->toBeTrue();
+    });
 });
--- tests/Unit/AuthentificationTest.php
+++ tests/Unit/AuthentificationTest.php
@@ -5,16 +5,18 @@ declare(strict_types=1);
 use FFTTApi\FFTTApi;
 use FFTTApi\Tests\HttpClientMock;
 
-it("devrait s'authentifier avec les bons identifiants", function (): void {
-    $api = FFTTApi::create(appId: '', appKey: '', serial: '1234', httpClient: HttpClientMock::class);
-    $result = $api->authentification->authentifier();
+describe('[xml_initialisation]', function (): void {
+    it("devrait s'authentifier avec les bons identifiants", function (): void {
+        $api = FFTTApi::create(appId: '', appKey: '', serial: '1234', httpClient: HttpClientMock::class);
+        $result = $api->authentification->authentifier();
 
-    expect($result)->toBeTrue();
-});
+        expect($result)->toBeTrue();
+    });
 
-it("devrait lever une exception avec les mauvais identifiants", function (): void {
-    $api = FFTTApi::create(appId: '', appKey: '', serial: '6789', httpClient: HttpClientMock::class);
+    it("devrait lever une exception avec les mauvais identifiants", function (): void {
+        $api = FFTTApi::create(appId: '', appKey: '', serial: '6789', httpClient: HttpClientMock::class);
 
-    expect(fn () => $api->authentification->authentifier())
-        ->toThrow(\FFTTApi\Exception\HttpException::class, 'Compte incorrect');
+        expect(fn () => $api->authentification->authentifier())
+            ->toThrow(\FFTTApi\Exception\HttpException::class, 'Compte incorrect');
+    });
 });
