b267d08c3364d1d8507cd5461b041581e85ba1c7
Aurélien Devaux <github@aureldvx.net>
2026-03-20 11:43:59 +0100

    tests: organismes

--- tests/Unit/OrganismeTest.php
+++ tests/Unit/OrganismeTest.php
@@ -8,31 +8,45 @@ use FFTTApi\Model\Organisme\Organisme;
 use FFTTApi\Tests\HttpClientMock;
 
 beforeEach(function (): void {
-    $this->api = FFTTApi::create('', '', '', new HttpClientMock);
+    $this->api = FFTTApi::create(appId: '', appKey: '', serial: '1234', httpClient: HttpClientMock::class);
 });
 
-it("devrait récupérer les organismes d'un type particulier", function (): void {
-    $result = $this->api->organisme->organismesParType(TypeOrganisme::ZONE);
+describe('[xml_organisme]', function (): void {
+    it("devrait récupérer les organismes d'un type particulier", function (): void {
+        $result = $this->api->organisme->organismesParType(TypeOrganisme::ZONE);
 
-    expect($result)->toBeArray()
-        ->and($result)->toHaveCount(7)
-        ->and($result[0])->toBeInstanceOf(Organisme::class)
-        ->and($result[0]->code())->toBe('Z01');
-});
+        expect($result)->toBeArray()
+            ->and($result)->toHaveCount(7)
+            ->and($result[0])->toBeInstanceOf(Organisme::class)
+            ->and($result[0]->code())->toBe('Z01');
+    });
 
-it("devrait récupérer un organisme par son code", function (): void {
-    $result = $this->api->organisme->organisme('D16');
+    it("devrait récupérer un organisme par son code", function (): void {
+        $result = $this->api->organisme->organisme('D16');
 
-    expect($result)
-        ->toBeInstanceOf(Organisme::class)
-        ->and($result->id())->toBe(131);
-});
+        expect($result)
+            ->toBeInstanceOf(Organisme::class)
+            ->and($result->id())->toBe(131);
+    });
 
-it("devrait récupérer les enfants d'un organisme", function (): void {
-    $result = $this->api->organisme->organismesEnfants('FEDE');
+    it("devrait récupérer les enfants d'un organisme", function (): void {
+        $result = $this->api->organisme->organismesEnfants('FEDE');
 
-    expect($result)->toBeArray()
-        ->and($result)->toHaveCount(7)
-        ->and($result[0])->toBeInstanceOf(Organisme::class)
-        ->and($result[0]->code())->toBe('Z01');
+        expect($result)->toBeArray()
+            ->and($result)->toHaveCount(7)
+            ->and($result[0])->toBeInstanceOf(Organisme::class)
+            ->and($result[0]->code())->toBe('Z01');
+    });
+
+    it("devrait retourner null si aucun organisme trouvé", function (): void {
+        $result = $this->api->organisme->organisme('B');
+
+        expect($result)->toBeNull();
+    });
+
+    it("devrait retourner un tableau vide d'enfants si aucun organisme père trouvé", function (): void {
+        $result = $this->api->organisme->organismesEnfants('B');
+
+        expect($result)->toBeArray()->and($result)->toHaveCount(0);
+    });
 });
